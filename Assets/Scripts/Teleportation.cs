b95982793e04600eb46a9d4a000913c096d8706d
GuillaumeGauduchon <guillaume.gauduchon@hotmail.com>
2021-05-05 11:54:45 +0200

    Last Update
    Ajout des MelonBird Trees
    Tentative de FX sur eux, Zone TP active; Mort des MelonBird Active

--- Assets/Scripts/Teleportation.cs
+++ Assets/Scripts/Teleportation.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class Teleportation : MonoBehaviour
 {
     public Transform Tp_Target;
+    public bool isKillable =false;
     
     void OnTriggerEnter(Collider other) {
         if(Tp_Target != null) {
             other.gameObject.transform.position = Tp_Target.position;
+        
+        if (isKillable) {
+            Destroy(gameObject);
+        }
         }
         else{
             Debug.LogWarning("Il n'y a pas de cible de téléportation !");
